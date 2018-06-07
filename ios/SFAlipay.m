cc6f6343982fb60c838cf69d323c877bfc1a865d
lzw <lzw@LZWdeMac-mini.local>
2018-06-07 09:32:28 +0800

    1.08

--- ios/SFAlipay.m
+++ ios/SFAlipay.m
@@ -24,19 +24,19 @@ RCT_EXPORT_MODULE();
     if ([notification.userInfo[@"respCode"]integerValue]==9000) {
         NSLog(@"支付成功");
         [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
-                                                     body:@{@"errCode ": @"9000",@"errMessage": @"支付成功"}];
+                                                     body:@{@"errCode": @"9000",@"errMessage": @"支付成功"}];
     }else if([notification.userInfo[@"respCode"]integerValue]==6001){
         NSLog(@"用户中途取消");
         [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
-                                                     body:@{@"errCode ": @"6001",@"errMessage": @"用户中途取消"}];
+                                                     body:@{@"errCode": @"6001",@"errMessage": @"用户中途取消"}];
     }else if([notification.userInfo[@"respCode"]integerValue]==6002){
         NSLog(@"网络连接出错");
         [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
-                                                     body:@{@"errCode ": @"6002",@"errMessage": @"网络连接错误"}];
+                                                     body:@{@"errCode": @"6002",@"errMessage": @"网络连接错误"}];
     }else if([notification.userInfo[@"respCode"]integerValue]==4000){
         NSLog(@"支付订单失败");
         [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
-                                                     body:@{@"errCode ": @"4000",@"errMessage": @"支付订单失败"}];
+                                                     body:@{@"errCode": @"4000",@"errMessage": @"支付订单失败"}];
     }
 }
 RCT_EXPORT_METHOD(registerApp:(NSDictionary*)dic){
@@ -48,6 +48,19 @@ RCT_EXPORT_METHOD(registerApp:(NSDictionary*)dic){
 RCT_EXPORT_METHOD(pay:(NSDictionary *)property){
         // NOTE: 调用支付结果开始支付
         [[AlipaySDK defaultService] payOrder:[property objectForKey:@"orderInfo"] fromScheme:self.appScheme callback:^(NSDictionary *resultDic) {
+            if ([[resultDic objectForKey:@"resultStatus"] isEqualToString:@"9000"]){
+                [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
+                                                             body:@{@"errCode": @"9000",@"errMessage": @"支付成功"}];
+            }else if([[resultDic objectForKey:@"resultStatus"] isEqualToString:@"6001"]){
+                [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
+                                                             body:@{@"errCode": @"6001",@"errMessage": @"用户中途取消"}];
+            }else if([[resultDic objectForKey:@"resultStatus"] isEqualToString:@"6002"]){
+                [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
+                                                             body:@{@"errCode": @"6002",@"errMessage": @"网络连接错误"}];
+            }else if([[resultDic objectForKey:@"resultStatus"] isEqualToString:@"4000"]){
+                [self.bridge.eventDispatcher sendAppEventWithName:@"AliResp"
+                                                             body:@{@"errCode": @"4000",@"errMessage": @"支付订单失败"}];
+            }
             NSLog(@"reslut = %@",resultDic);
         }];
 }
